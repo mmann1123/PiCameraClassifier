2330c0e0c25f6d3636fa4b7ec37dd1a7f26ae4fe
Mike <mmann1123@gmail.com>
2018-05-01 16:39:33 -0400

    added accuracy assessment fixed adding new labels to predicted label csv

--- Retrain_classifier.py
+++ Retrain_classifier.py
@@ -3,7 +3,7 @@
 #source activate tensorflow   # activate anaconda environment with tensorflow
 IMAGE_SIZE=224
 ARCHITECTURE="mobilenet_1.0_${IMAGE_SIZE}"
-python -m scripts.retrain   --bottleneck_dir=tf_files/bottlenecks    --model_dir=tf_files/models/   --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}"   --output_graph=tf_files/retrained_graph.pb   --output_labels=tf_files/retrained_labels.txt      --image_dir=/home/mmann1123/Dropbox/PiCapture/StreetCapture --random_brightness=15
+python -m scripts.retrain   --bottleneck_dir=tf_files/bottlenecks    --model_dir=tf_files/models/   --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}"   --output_graph=tf_files/retrained_graph.pb   --output_labels=tf_files/retrained_labels.txt      --image_dir=/home/mmann1123/Dropbox/PiCapture/StreetCapture --random_brightness=5  --architecture="${ARCHITECTURE}" --how_many_training_steps=2000
 
 #removign architecture flag with default to inception v3 
 
